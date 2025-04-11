1488c458d5a9ce60d9bb67781b17c7888256ce5e
aman-008 <amanp9568@gmail.com>
2025-04-11 17:45:09 +0530

    Converted submodule to regular folder

---
+++ Python/02/main.py
@@ -0,0 +1,38 @@
+# Integer
+integer_var = 42
+
+# Float
+float_var = 3.14
+
+# String
+string_var = "Hello, World!"
+
+# Boolean
+boolean_var = True
+
+# List
+list_var = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_var = (1, 2, 3, 4, 5)
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+
+# Dictionary
+dict_var = {"key1": "value1", "key2": "value2"}
+
+# NoneType
+none_var = None
+
+# Complex
+complex_var = 2 + 3j
+
+# Bytes
+bytes_var = b"Hello"
+
+# Bytearray
+bytearray_var = bytearray(5)
+
+# Range
+range_var = range(10)
---
+++ Python/02/practice.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+x = 56
+y = 45
+z = x+y
+print(z)
