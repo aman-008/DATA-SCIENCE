aa012256c0b594882c4ab0951588ca1e2ed42ba8
aman-008 <amanp9568@gmail.com>
2025-04-11 18:36:53 +0530

    commit

---
+++ Python/09/pass.py
@@ -0,0 +1,17 @@
+# Placeholder in a class
+class MyClass:
+    pass  # Class implementation will be added later
+
+# Using pass in a loop
+for i in range(5):
+    if i % 2 == 0:
+        pass  # Skip even numbers for now
+    else:
+        print(f"Odd number: {i}")
+
+# Using pass in a conditional statement
+x = 10
+if x > 5:
+    pass  # Logic for x > 5 will be added later
+else:
+    print("x is less than or equal to 5")
