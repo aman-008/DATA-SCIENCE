a67cf4dfe5b72a5a25787068295bb427776c911e
aman-008 <amanp9568@gmail.com>
2025-04-14 03:49:00 +0530

    projects

---
+++ Python/21_Projects/Guess_The_Number/main.py
@@ -0,0 +1,16 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    
+    guess = 0
+    
+    while guess != random_number:
+        guess = int(input(f"Guess the number between 1 to {x}: "))
+        if guess < random_number:
+            print("Sorry, Guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, Guess again. Too high")
+    print(f"Yay, congrats. You have guessed the number {random_number}.")
+
+guess(10)
